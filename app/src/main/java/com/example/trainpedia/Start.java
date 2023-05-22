8a6439df7a02dc83ef2a298241dd881f2ca1bebe
Roofiif Ananta Fathan <roofiif2000018419@webmail.uad.ac.id>
2023-05-22 20:30:31 +0700

    Test

--- app/src/main/java/com/example/trainpedia/Start.java
+++ app/src/main/java/com/example/trainpedia/Start.java
@@ -2,13 +2,27 @@ package com.example.trainpedia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Start extends AppCompatActivity {
+    Button btnstr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        btnstr = (Button) findViewById(R.id.button2);
+        btnstr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent inte = new Intent(Start.this, Login.class);
+                startActivity(inte);
+            }
+        });
+
     }
+
 }
