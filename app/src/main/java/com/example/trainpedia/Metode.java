1ab8e63511d12c96a95d642cdbb7034914ee6f55
Ahmad Rizgifan Fauzhy <ahmad2000018436@webmail.uad.ac.id>
2023-06-27 10:39:32 +0700

    Trainpedia Commit

--- app/src/main/java/com/example/trainpedia/Metode.java
+++ app/src/main/java/com/example/trainpedia/Metode.java
@@ -7,24 +7,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class Login extends AppCompatActivity {
+public class Metode extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
-        Button button2 = findViewById(R.id.loginuser);
+        setContentView(R.layout.activity_metode);
+        Button button2 = findViewById(R.id.button3);
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Perform your desired action when the button is clicked
                 // For example, you can start a new activity:
-                startActivity(new Intent(Login.this, Home.class));
+                startActivity(new Intent(Metode.this, Checkout.class));
             }
         });
-    }
-    public void admin (View v){
-        startActivity(new Intent(Login.this, MakeAcc.class));
     }
 }
--- app/src/main/java/com/example/trainpedia/Start.java
+++ app/src/main/java/com/example/trainpedia/Start.java
@@ -1,14 +1,26 @@
-package com.example.trainpedia;
+    package com.example.trainpedia;
 
-import androidx.appcompat.app.AppCompatActivity;
+    import androidx.appcompat.app.AppCompatActivity;
+    import android.content.Intent;
+    import android.os.Bundle;
+    import android.view.View;
+    import android.widget.Button;
 
-import android.os.Bundle;
+    public class Start extends AppCompatActivity {
 
-public class Start extends AppCompatActivity {
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_start);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start);
+            Button button2 = findViewById(R.id.button2);
+            button2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // Perform your desired action when the button is clicked
+                    // For example, you can start a new activity:
+                    startActivity(new Intent(Start.this, Login.class));
+                }
+            });
+        }
     }
-}
