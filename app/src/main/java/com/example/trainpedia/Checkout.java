71f7ebd2f1641933b9e6bf87e155ff61728b8807
Ahmad Rizgifan Fauzhy <ahmad2000018436@webmail.uad.ac.id>
2023-07-01 15:03:34 +0700

    Trainpedia Commit

--- app/src/main/java/com/example/trainpedia/Checkout.java
+++ app/src/main/java/com/example/trainpedia/Checkout.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Checkout extends AppCompatActivity {
 
@@ -14,12 +15,21 @@ public class Checkout extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout);
         Button button2 = findViewById(R.id.button);
+        ImageButton imageButton = findViewById(R.id.imageButton);
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Perform your desired action when the button is clicked
                 // For example, you can start a new activity:
                 startActivity(new Intent(Checkout.this, Metode.class));
+            }
+        });
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start the new activity
+                Intent intent = new Intent(Checkout.this, Home.class);
+                startActivity(intent);
             }
         });
     }
--- app/src/main/java/com/example/trainpedia/Metode.java
+++ app/src/main/java/com/example/trainpedia/Metode.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+
 import com.midtrans.sdk.corekit.models.snap.TransactionResult;
 import com.midtrans.sdk.corekit.callback.TransactionFinishedCallback;
 import com.midtrans.sdk.corekit.core.MidtransSDK;
@@ -21,12 +23,21 @@ public class Metode extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_metode);
         Button button2 = findViewById(R.id.button3);
+        ImageButton imageButton = findViewById(R.id.imageButton);
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Perform your desired action when the button is clicked
                 // For example, you can start a new activity:
                 startActivity(new Intent(Metode.this, Checkout.class));
+            }
+        });
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start the new activity
+                Intent intent = new Intent(Metode.this, Checkout.class);
+                startActivity(intent);
             }
         });
     }
